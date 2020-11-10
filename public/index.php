63e76a0dc990281eb1724b71b4b02ab1569858df
JinMurayama <shining.hart@gmail.com>
2020-11-10 10:35:13 +0900

    update index.php

--- public/index.php
+++ public/index.php
@@ -40,7 +40,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 /*
 |--------------------------------------------------------------------------
 | Run The Application
-|--------------------------------------------------------------------------|
+|--------------------------------------------------------------------------
+|
 | Once we have the application, we can handle the incoming request
 | through the kernel, and send the associated response back to
 | the client's browser allowing them to enjoy the creative
